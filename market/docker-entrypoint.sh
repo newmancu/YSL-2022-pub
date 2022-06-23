1df74ac896831e9fd64654d5e5c389012144a212
newmancu <newmancu@gmail.com>
2022-06-23 22:10:51 +0300

    fix

--- market/docker-entrypoint.sh
+++ market/docker-entrypoint.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 echo "CREATE SECRET_KEY"
-python ./market/create_secret.py
+python ./create_secret.py
 
 echo "Creating migrations"
 
-while ! python ./market/manage.py makemigrations 2>&1; do
+while ! python ./manage.py makemigrations 2>&1; do
   echo "python manage.py makemigrations"
   sleep 1
 done
 
-while ! python ./market/manage.py makemigrations back_api 2>&1; do
+while ! python ./manage.py makemigrations back_api 2>&1; do
   echo "python manage.py makemigrations"
   sleep 1
 done
 
 echo "Migrate the Database at startup of project"
 
-while ! python ./market/manage.py migrate  2>&1; do
+while ! python ./manage.py migrate  2>&1; do
    echo "Migration is in progress status"
    sleep 1
 done
 
-while ! python ./market/manage.py migrate back_api 2>&1; do
+while ! python ./manage.py migrate back_api 2>&1; do
    echo "Migration is in progress status"
    sleep 1
 done
