29b0ca16ca57c4b29188677c7623f799984d0e53
newmancu <newmancu@gmail.com>
2022-06-23 22:59:50 +0300

    Readme.md

--- market/market/urls.py
+++ market/market/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),    # default django admin page
+    # path('admin/', admin.site.urls),    # default django admin page
     path('', include('back_api.urls'))
 ]
