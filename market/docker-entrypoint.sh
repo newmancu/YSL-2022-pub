e45a7786d09c01b56094ffbbe8f80f620b085897
newmancu <newmancu@gmail.com>
2022-06-23 21:18:15 +0300

    fix

--- market/docker-entrypoint.sh
+++ market/docker-entrypoint.sh
@@ -7,14 +7,24 @@ echo "Creating migrations"
 
 while ! python ./market/manage.py makemigrations 2>&1; do
   echo "python manage.py makemigrations"
-  sleep 3
+  sleep 1
+done
+
+while ! python ./market/manage.py makemigrations back_api 2>&1; do
+  echo "python manage.py makemigrations"
+  sleep 1
 done
 
 echo "Migrate the Database at startup of project"
 
 while ! python ./market/manage.py migrate  2>&1; do
    echo "Migration is in progress status"
-   sleep 3
+   sleep 1
+done
+
+while ! python ./market/manage.py migrate back_api 2>&1; do
+   echo "Migration is in progress status"
+   sleep 1
 done
 
 
